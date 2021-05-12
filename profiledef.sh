df4aebfbc8253ab749d3b6f8d5fbbadc00b87d09
rtxx <ruiteixeira906@gmail.com>
2021-05-12 17:34:32 +0000

    Add files via upload

---
+++ profiledef.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="arch-i3"
+iso_label="ARCHI3_$(date +%Y%m)"
+iso_publisher="rtxx (github.com/rtxx)"
+iso_application="Arch with my i3 configs"
+iso_version="0.1"
+install_dir="arch"
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+
+)
