5c7636683953beb80b850bedd41a85dffac4cea6
rtxx <ruiteixeira906@gmail.com>
2022-03-04 15:29:33 +0000

    Misc update
    Changed jgmenu default terminal, fixed emojipick, added xclip to packages (emojipick uses it), added execute permission to a bunch of scripts

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -14,7 +14,17 @@ set -e -u
 systemctl enable cups
 
 # set permission to execute on i3 scripts
-chmod +x /etc/skel/.config/i3/config.d/scripts/*
+chmod +x /etc/skel/.config/i3/config.d/scripts/i3sounds/i3sounds
+chmod +x /etc/skel/.config/i3/config.d/scripts/packy/packy
+chmod +x /etc/skel/.config/i3/config.d/scripts/sunset/sunset
+chmod +x /etc/skel/.config/i3/config.d/scripts/check-updates
+chmod +x /etc/skel/.config/i3/config.d/scripts/i3cpu-governor
+chmod +x /etc/skel/.config/i3/config.d/scripts/i3exit
+chmod +x /etc/skel/.config/i3/config.d/scripts/i3services
+chmod +x /etc/skel/.config/i3/config.d/scripts/emojipick
+chmod +x /etc/skel/.config/i3/config.d/scripts/emojiget.py
+chmod +x /etc/skel/.config/i3/config.d/scripts/feh-blur
+chmod +x /etc/skel/.config/i3/config.d/scripts/help-pacman
 
 # set permission to execute on i3 remake script
 chmod +x /etc/skel/.config/i3/makeconfig
